274d8fadd8c88a31e8900dc632b832ff94086e6c
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-07-19 10:06:11 +0800

    Create cart.py

---
+++ cart.py
@@ -0,0 +1,13 @@
+store = "Harry Shopping Mall"
+product = [] #create 5 products you will see in a shopping mall, remember to use string for all
+cart = []
+print(store)
+print(product, '\n')
+while True:
+  msg = input("Please enter what you want to buy (q=Quit) : ")
+  if #if the user enter q or Q, the loop should break
+  #how to break the loop
+  else:
+    if #if the user enter the product in product list
+    #how to add the product in cart
+print("You have bought :", cart)
